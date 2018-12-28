6418479dce5bfffcc75dcf17b0bd987b1af667f7
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-28 02:38:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,42 +35,41 @@ class Student
       student
   end
 
-  def self.find_by_name(name)
-      sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-      SQL
+    def self.find_by_name(name)
+        sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE name = ?
+    
+        SQL
 
-      DB[:conn].execute(sql, name).map do |row|
-          self.new_from_db(row)
-      end.first
-  end
+        DB[:conn].execute(sql, name).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
 
-  def self.new_from_db(row)
-      student = self.new
-      student.id = row[0]
-      student.name = row[1]
-      student.grade = row[2]
-      student
-  end
+    def self.new_from_db(row)
+        student = self.new
+        student.id = row[0]
+        student.name = row[1]
+        student.grade = row[2]
+        student
+    end
 
-  def update
-      sql = "UPDATE students SET name = ?, grade=? WHERE id = ?"
-      DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
+    def update
+        sql = "UPDATE students SET name = ?, grade=? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.grade, self.id)
+    end
 
-  def save
-      if self.id
+    def save
+        if self.id
           self.update
-      else
-      sql = <<-SQL
-        INSERT INTO students(name, grade) VALUES (?, ?)
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end
-  end
+        else
+            sql = <<-SQL
+            INSERT INTO students(name, grade) VALUES (?, ?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.grade)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        end
+    end
 end
