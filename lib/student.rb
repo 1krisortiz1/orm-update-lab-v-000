ca524fbfd706b1e3fdb9603bbcfcf7bdbe98dd33
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-28 02:32:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,12 @@ class Student
   end
 
   def self.find_by_name(name)
+      sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name = ?
+      Limite 1
+      SQL
   end
 
   def self.new_from_db(row)
