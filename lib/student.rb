c3e285807b67439af30418ae122048a4e74afba6
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-28 02:32:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,11 +37,15 @@ class Student
 
   def self.find_by_name(name)
       sql = <<-SQL
-      SELECT * 
+      SELECT *
       FROM students
       WHERE name = ?
       Limite 1
       SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+          self.new_from_db(row)
+      end.first
   end
 
   def self.new_from_db(row)
